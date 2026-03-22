9b3bc6207ec157b14f3ea8d629a4d1168ebb1e81
gimhaninavodya <146187436+gimhaninavodya@users.noreply.github.com>
2026-03-22 19:10:46 +0530

    update eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,16 @@
 import js from "@eslint/js";
+import globals from "globals";
 
 export default [
     js.configs.recommended,
     {
         files: ["src/**/*.js"],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.commonjs,
+            },
+        },
         rules: {
             "no-unused-vars": "warn",
             "no-console": "off",
